e1abdc4619bbbae16601306894109dc84b87b2b2
Mateus-Redivo <ms.redivo@gmail.com>
2021-08-05 15:21:55 -0300

    Adição da condição

--- Trabalho_Vania.cpp
+++ Trabalho_Vania.cpp
@@ -8,26 +8,36 @@ int main()
 {
 	setlocale(LC_ALL,"portuguese");
 	int Horas[0], Dias[0];
-	string Orientador[0], Membro1[0],Membro2[0];
+	string Orientador[0], Membro1[0],Membro2[0], Projeto[0];
 	
-	cout<<"Iforme o nome do orientador";
+	cout<<"Informe o nome do orientador: ";
 	cin>>Orientador[1];
-	cout<<"\nIforme o nome do primeiro convidado";
+	cout<<"\nInforme o nome do primeiro convidado: ";
 	cin>>Membro1[1];
-	cout<<"\nIforme o nome do segundo convidado";
+	cout<<"\nInforme o nome do segundo convidado: ";
 	cin>>Membro2[1];
 	
-	
-		cout<<"\nInforme a hora: ";
-		cin>>Horas[0];
+	cout<<"\nInforme a hora: ";
+	cin>>Horas[0];
 		if(Horas[0] > 23)
 		{
 		   	system("cls");
 			cout<<"Informe uma hora válida";
-		}
+		}	
 		else
 		{
-			cout<<"\nInforme o membro: ";
-  	   		cout<<"\nDigite seu nome: ";
-		}	
+			cout<<"\nInforme o dia: ";
+			cin>>Dias[0];
+			if(Dias[0] > 31)
+			{
+				system("cls");
+				cout<<"Informe um dia válido";
+			}
+			else
+			{
+				cout<<"\nInforme o projeto: ";
+				cin>>Projeto[1];
+			}
+		}
+  	   	
 }
