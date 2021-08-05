915d11224677b9ea44f58f939031b9235cc67bfb
Mateus-Redivo <ms.redivo@gmail.com>
2021-08-05 14:37:17 -0300

    Bancas

--- Trabalho_Vania.cpp
+++ Trabalho_Vania.cpp
@@ -8,19 +8,26 @@ int main()
 {
 	setlocale(LC_ALL,"portuguese");
 	int Horas[0], Dias[0];
-
+	string Orientador[0], Membro1[0],Membro2[0];
 	
-	cout<<"\nInforme a hora: ";
+	cout<<"Iforme o nome do orientador";
+	cin>>Orientador[1];
+	cout<<"\nIforme o nome do primeiro convidado";
+	cin>>Membro1[1];
+	cout<<"\nIforme o nome do segundo convidado";
+	cin>>Membro2[1];
+	
+	
+		cout<<"\nInforme a hora: ";
 		cin>>Horas[0];
 		if(Horas[0] > 23)
 		{
 		   	system("cls");
 			cout<<"Informe uma hora válida";
 		}
-	cout<<"\nInforme o membro: ";
-	cout<<"\nDigite seu nome: ";
-	
-
-	
+		else
+		{
+			cout<<"\nInforme o membro: ";
+  	   		cout<<"\nDigite seu nome: ";
+		}	
 }
-
